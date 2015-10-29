3f0f87724abcdd1d0550fb4b921b440c23414472
Mio <mio.bluewatermelon@gmail.com>
2015-10-29 18:07:26 +0100

    gestion fav & blacklist avec localstorage

--- www/js/app.js
+++ www/js/app.js
@@ -82,4 +82,20 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ionic.contrib.ui.tin
       });
     }
   }
-});
+})
+.factory('$localstorage', ['$window', function($window) {
+  return {
+    set: function(key, value) {
+      $window.localStorage[key] = value;
+    },
+    get: function(key, defaultValue) {
+      return $window.localStorage[key] || defaultValue;
+    },
+    setObject: function(key, value) {
+      $window.localStorage[key] = JSON.stringify(value);
+    },
+    getObject: function(key) {
+      return JSON.parse($window.localStorage[key] || '{}');
+    }
+  }
+}]);
